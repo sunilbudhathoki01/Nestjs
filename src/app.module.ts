218f7594355e10e52a0b3b45d6ee2fd7db199a31
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2026-04-11 13:28:27 +0545

    feat:add crud routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProfilesModule } from './profiles/profiles.module';
 import { ProfileController } from './profile/profile.controller';
+import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [ProfilesModule],
   controllers: [AppController, ProfileController],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,10 +1,57 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
-@Controller('profiles')
+@Controller('profile')
 export class ProfilesController {
-  @Get('alldata')
-  findAll(@Query('age') age: number) {
-    // return 'hello world sunil vai k xa';
-    return [{ age }];
+  // Query
+  @Get('sunil')
+  findAll(@Query('location') location: string) {
+    return [{ location }];
+  }
+  // param id
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return [{ id }];
+  // }
+  // return single profile
+  // it should take an id param and return the object with that id
+  @Get(':id')
+  findProfile(@Param('id') id: string) {
+    return { id };
+  }
+  // post route practice
+  @Post()
+  createRoute(@Body() createProfileDto: CreateProfileDto) {
+    return {
+      name: createProfileDto.name,
+      description: createProfileDto.description,
+    };
+  }
+
+  // update
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateProfileDto: UpdateProfileDto) {
+    return {
+      id,
+      ...updateProfileDto,
+    };
+  }
+  // delete
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id') id: string) {
+    return { id };
   }
 }
