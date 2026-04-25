e4ba10929bb028d4219b2efb3cc3cf72ed73232b
sunil budhathoki <ashishbudhathoki952@gmail.com>
2026-04-25 13:33:32 +0545

    feat:handle exception globally

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,5 +13,6 @@ async function bootstrap() {
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalFilters(new GlobalExceptionFilter());
 }
 bootstrap();
